a0952089428f17b1972bc0960b944a52e1e6fe1f
Grace Tveit <grace.tveit@gmail.com>
2022-04-14 13:38:36 -0700

    docs; adding notes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use rltk::{Rltk, GameState};
+use rltk::{GameState, Rltk};
 
-struct State {}
+struct State {} // Defines the struct STtate
+
 impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
+    // Implements the *trait* GameState onto the State struct
+    fn tick(&mut self, ctx: &mut Rltk) {
+        // tick is a special function that is called every frame the GameState
+        // is running
         ctx.cls();
+        // Generally speaking, you want to clear the screen at the beginning of
+        // a frame, unless you specifically don't want to
         ctx.print(1, 1, "Hello Rust World");
     }
 }
@@ -13,6 +19,6 @@ fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
         .with_title("Roguelike Tutorial")
         .build()?;
-    let gs = State{ };
+    let gs = State {};
     rltk::main_loop(context, gs)
 }
